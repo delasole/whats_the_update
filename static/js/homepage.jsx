ddf7e4041133535f7a32401f2ef8ba9e36e198e3
Donnisha Frison <dfri1225@gmail.com>
2020-12-02 04:51:04 +0000

    Fixed react code for table grid

--- static/js/homepage.jsx
+++ static/js/homepage.jsx
@@ -1,16 +1,6 @@
 function Order(props) {
   const {order} = props;
   return (
-  <table className="min-w-lg divide-y divide-gray-200">
-      <thead>
-        <tr>
-          <td scope="col" className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Order Id</td>
-          <td scope="col" className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Order Date</td>
-          <td scope="col" className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Buyer's Name</td>
-          <td scope="col" className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Item Purchase</td>
-        </tr>
-      </thead>
-      <tbody className="bg-white divide-y divide-gray-200">
         <tr>
           <td className="px-4 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
           <a className="text-blue-500" href={"/messagecenter/" + order.order_id}>{order.order_id}</a>
@@ -19,8 +9,6 @@ function Order(props) {
           <td className="px-4 py-4 whitespace-nowrap text-sm text-gray-500">{order.first_name} {order.last_name}</td>
           <td className="px-4 py-4 whitespace-nowrap text-sm text-gray-500">{order.item}</td>
         </tr>
-      </tbody>
-  </table>  
   );
 }
 
@@ -40,9 +28,19 @@ function OrderHistory() {
     content.push(<Order key ={order.order_id} order={order}/>);
   }
   return (
-  <div>
+    <table className="min-w-lg divide-y divide-gray-200">
+    <thead>
+      <tr>
+        <td scope="col" className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Order Id</td>
+        <td scope="col" className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Order Date</td>
+        <td scope="col" className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Buyer's Name</td>
+        <td scope="col" className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Item Purchased</td>
+      </tr>
+    </thead>
+    <tbody className="bg-white divide-y divide-gray-200">
     {content}
-  </div>
+  </tbody>
+  </table>
     );
 }
 
